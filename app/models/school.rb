fef85db5a59b0289d69f16992b024329f154ecae
DanielJunior <danieljunior@id.uff.br>
2016-12-06 16:09:46 -0200

    Trying make geocode work

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,11 @@
 class School < ApplicationRecord
-  reverse_geocoded_by :latitude, :longitude
-  after_validation :reverse_geocode  # auto-fetch address
-  
+  geocoded_by :full_street_address
+  after_validation :geocode # auto-fetch coordinates
+
+  def full_street_address
+    [address, city, state, country].compact.join(', ')
+  end
+
   def near_by kilometers
     miles = kilometers / 1.609344
     return nearbys(miles)
