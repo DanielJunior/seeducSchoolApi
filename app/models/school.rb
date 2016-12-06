0f2d3c29e94eb76770a5421d09dae46b2bfa3001
DanielJunior <danieljunior@id.uff.br>
2016-12-06 15:46:44 -0200

    Change attributes to geocode

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,7 @@
 class School < ApplicationRecord
-  geocoded_by :full_street_address # can also be an IP address
-  after_validation :geocode # auto-fetch coordinates
-
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
+  
   def near_by kilometers
     miles = kilometers / 1.609344
     return nearbys(miles)
