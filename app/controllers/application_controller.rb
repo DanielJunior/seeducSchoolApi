9770fed2f87cfbedc08ab0c6fda6bf8f1e9eb0c0
DanielJunior <danieljunior@id.uff.br>
2017-01-10 16:05:04 -0200

    Criando actions para procura por nome, despesa e receita

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,28 @@ class ApplicationController < ActionController::API
   def index
     render json: School.all
   end
+
+  def find
+    id = params[:id]
+    school = School.find(id)
+    render json: school
+  end
+
+  def findByName
+    name = params[:name]
+    schools = School.where("unidade_escolar like '%#{ActiveSupport::Inflector.transliterate(name).upcase}%'")
+    render json: schools
+  end
+
+  def findByExpense
+    expense = params[:expense]
+    schools = School.having("manuntencao_despesa + merenda_estadual_federal_despesa <= ?", expense).group(:id)
+    render json: schools
+  end
+
+  def findByIncome
+    income = params[:income]
+    schools = School.having("manuntencao_receita + merenda_estadual_federal_receita <= ?", income).group(:id)
+    render json: schools
+  end
 end
